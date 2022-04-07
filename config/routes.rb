5479f083c22b1133262332ceffb08f3ed462a302
r4v3n <r4v3n.art@gmail.com>
2022-04-07 16:30:15 -0400

    add controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root 'sessions#index'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resource :sessions, only: :create
+  resources :users
 end
---
+++ spec/requests/users_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe "Users", type: :request do
+  describe "GET /index" do
+    xit "returns http success" do
+    end
+  end
+
+  describe "GET /create" do
+    xit "returns http success" do
+    end
+  end
+
+  describe "GET /show" do
+    xit "returns http success" do
+    end
+  end
+end
