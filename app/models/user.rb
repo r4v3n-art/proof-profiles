a8bf72ce4ced772536769a743dec780d5d9d70a6
r4v3n <r4v3n.art@gmail.com>
2022-04-07 14:54:51 -0400

    add validation for eth address

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ApplicationRecord
   validates :eth_address, presence: true, uniqueness: true
+  validate :valid_eth_address?
+
+  private
+  def valid_eth_address?
+    if eth_address && !Eth::Utils.valid_address?(eth_address)
+      errors.add(:eth_address, I18n.t('errors.invalid_eth_address'))
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,5 +5,12 @@ RSpec.describe User, type: :model do
     subject { build(:user) }
     it { should validate_presence_of(:eth_address) }
     it { should validate_uniqueness_of(:eth_address) }
+
+    it 'should validate ETH address' do
+      user = build(:user, eth_address: 'fdafds')
+
+      expect(user.valid?).to be(false)
+      expect(user.errors.messages).to eq({ eth_address: [I18n.t('errors.invalid_eth_address')] })
+    end
   end
 end
